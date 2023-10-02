76a46cf3ed7b0810279f56ff3f162de0e9946627
Valeria <114831021+starryskylover@users.noreply.github.com>
2023-10-02 17:52:00 +0300

    Add files via upload

--- omc_combination.cpp
+++ omc_combination.cpp
@@ -11,10 +11,10 @@ void omc_combination() {
 
     Double_t weight_znunu,weight_gamma_jets, ph_pt, ph_phi, ph_eta, soft_term_pt, soft_term_phi, jet_lead_phi,jet_lead_pt, jet_lead_eta, metTST_pt, metTST_phi;
     Int_t n_jet;
-
+    Double_t koef_gamma_jets, koef_znunu, lumin, sumw_znunu, sumw_gamma_jets;
     string Z_nunu_tree_name = "Z_nunu_tree";
 
-    TFile* file_combined = new TFile ("/home/valerian/programs/atom_progs/combined.root", "RECREATE");
+    TFile* file_combined = new TFile ("/home/valerian/programs/atom_progs/omc/combined_a.root", "RECREATE");
     TTree *combined_tree = new TTree ("combined_tree", "combined_tree");
 
     combined_tree->Branch("ph_pt",&ph_pt,"ph_pt/D");
@@ -34,12 +34,17 @@ void omc_combination() {
 
     combined_tree->Branch("weight_znunu",&weight_znunu,"weight_znunu/D");
     combined_tree->Branch("weight_gamma_jets",&weight_gamma_jets,"weight_gamma_jets/D");
+    combined_tree->Branch("koef_znunu",&koef_znunu,"koef_znunu/D");
+    combined_tree->Branch("koef_gamma_jets",&koef_gamma_jets,"koef_gamma_jets/D");
+    combined_tree->Branch("lumin",&lumin,"lumin/D");
+    combined_tree->Branch("sumw_znunu",&sumw_znunu,"sumw_znunu/D");
+    combined_tree->Branch("sumw_gamma_jets",&sumw_gamma_jets,"sumw_gamma_jets/D");
 
 
+    Double_t sum=0;
 
-
-    TFile* file_gamma_jets = new TFile ("/home/valerian/programs/atom_progs/gamma_jets.root", "READ");
-    TFile* file_znunu = new TFile ("/home/valerian/programs/atom_progs/Z_nunu.root", "READ");
+    TFile* file_gamma_jets = new TFile ("/home/valerian/programs/atom_progs/omc/gamma_jets/gamma_jets_a/gamma_jets_a.root", "READ");
+    TFile* file_znunu = new TFile ("/home/valerian/programs/atom_progs/omc/Zj/Zj_a/Zj_a.root", "READ");
 
     TTree* tree_gamma_jets = (TTree*)file_gamma_jets->Get("gamma_tree");
     TTree* tree_znunu = (TTree*)file_znunu->Get("Z_nunu_tree");
@@ -61,6 +66,11 @@ void omc_combination() {
         TTreeReaderValue<Double_t> metTST_pt_gamma(*readers_gamma_jets[0], "metTST_pt_gamma");
         TTreeReaderValue<Double_t> metTST_phi_gamma(*readers_gamma_jets[0], "metTST_phi_gamma");
         TTreeReaderValue<UInt_t> n_jet_gamma(*readers_gamma_jets[0], "n_jet_gamma");
+        TTreeReaderValue<Double_t> koef_gamma(*readers_gamma_jets[0], "koef_gamma");
+        TTreeReaderValue<Double_t> luminosity(*readers_gamma_jets[0], "luminosity");
+        TTreeReaderValue<Double_t> sumw_gamma(*readers_gamma_jets[0], "sumw_gamma");
+
+
 
         TTreeReaderValue<Double_t> weight_Znunu(*readers_znunu[0], "weight_Znunu");
         TTreeReaderValue<Double_t> ph_pt_Znunu(*readers_znunu[0], "ph_pt_Znunu");
@@ -74,11 +84,15 @@ void omc_combination() {
         TTreeReaderValue<Double_t> metTST_pt_Znunu(*readers_znunu[0], "metTST_pt_Znunu");
         TTreeReaderValue<Double_t> metTST_phi_Znunu(*readers_znunu[0], "metTST_phi_Znunu");
         TTreeReaderValue<UInt_t> n_jet_Znunu(*readers_znunu[0], "n_jet_Znunu");
+        TTreeReaderValue<Double_t> koef_Znunu(*readers_znunu[0], "koef_Znunu");
+        TTreeReaderValue<Double_t> sumw_Znunu(*readers_znunu[0], "sumw_Znunu");
 
                 Int_t entries_gamma_jets = readers_gamma_jets[0] -> GetEntries();
 
                 // Int_t entities_gamma_jets = 20;
 
+                // Int_t start=0;
+                // Int_t end=readers_gamma_jets[0] -> GetEntries();
                 Int_t start;
                 Int_t end;
 
@@ -94,13 +108,23 @@ void omc_combination() {
                     cout << j << endl;
 
                     for (Int_t k = 0; k < readers_znunu[0] -> GetEntries(); k++) {
-                    // for (Int_t k = 0; k < 3; k++) {
+                    //for (Int_t k = 0; k < 3; k++) {
                         readers_znunu[0] -> SetEntry(k);
 
+                        // koef_gamma_jets=*koef_gamma;
+                        // koef_znunu=*koef_Znunu;
+                        // lumin=*luminosity;
+                        // sumw_gamma_jets=*sumw_gamma;
+                        // sumw_znunu=*sumw_Znunu;
+
+
+
+                        // std::cout << "koef_gamma=" <<   koef_gamma_jets << '\n';
+                        // std::cout << "luminosity=" <<   lumin << '\n';
                         // TLorentzVector *jet_lead_z= new TLorentzVector();
                         // TLorentzVector *jet_lead_g= new TLorentzVector();
-                        TLorentzVector *jet_lead= new TLorentzVector();
-                        TLorentzVector *ph_g= new TLorentzVector();
+                        TLorentzVector *jet_lead = new TLorentzVector();
+                        TLorentzVector *ph_g = new TLorentzVector();
                         // jet_lead_z->SetPtEtaPhiM(*jet_lead_pt_Znunu, *jet_lead_eta_Znunu, *jet_lead_phi_Znunu, 0);
                         // jet_lead_g->SetPtEtaPhiM(*jet_lead_pt_gamma, *jet_lead_eta_gamma, *jet_lead_phi_gamma, 0);
                         ph_g->SetPtEtaPhiM(*ph_pt_gamma, *ph_eta_gamma, *ph_phi_gamma, 0);
@@ -153,12 +177,10 @@ void omc_combination() {
                         TLorentzVector *metTST = new TLorentzVector();
                         TLorentzVector *metTST_z = new TLorentzVector();
                         TLorentzVector *metTST_g = new TLorentzVector();
-                        TLorentzVector *ph_z = new TLorentzVector();
                         metTST_z->SetPtEtaPhiM(*metTST_pt_Znunu, 0, *metTST_phi_Znunu, 0);
                         metTST_g->SetPtEtaPhiM(*metTST_pt_gamma, 0, *metTST_phi_gamma, 0);
-                        ph_z->SetPtEtaPhiM(*ph_pt_Znunu, 0, *ph_phi_Znunu, 0);
 
-                        *metTST=*metTST_g+*ph_z+*metTST_z;
+                        *metTST=*metTST_g+*metTST_z;
                         metTST_pt=(*metTST).Pt();
                         metTST_phi=(*metTST).Phi();
 
@@ -178,9 +200,11 @@ void omc_combination() {
                         if (dphi_met_gamma <= 0.7) continue;
                         if (dphi_met_jet_lead <= 0.4 && n_jet == 1) continue;
                         if (soft_term_pt >= 16) continue;
+
                         weight_znunu=*weight_Znunu;
                         weight_gamma_jets=*weight_gamma;
 
+                        sum=weight_znunu*weight_gamma_jets;
                         combined_tree -> Fill();
 
                     }
